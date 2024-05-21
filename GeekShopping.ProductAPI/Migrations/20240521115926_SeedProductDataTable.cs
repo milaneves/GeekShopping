40f473099a4842c60de747882cac8a6020c81270
Mila Neves <mila.neves@live.com>
2024-05-21 09:33:35 -0300

    create-product-microsservice

---
+++ GeekShopping.ProductAPI/Migrations/20240521115926_SeedProductDataTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 2L, "T-shirt", "It is estabilished fact that a read", "teste", "Name", 69.9m },
+                    { 3L, "T-shirt", "It is estabilished fact that a read", "teste", "Name", 69.9m },
+                    { 4L, "T-shirt", "It is estabilished fact that a read", "teste", "Name", 69.9m },
+                    { 5L, "T-shirt", "It is estabilished fact that a read", "teste", "Name", 69.9m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+        }
+    }
+}
---
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(){}
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options){}
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "It is estabilished fact that a read",
+                ImagemURL = "teste",
+                CategoryName = "T-shirt"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3 ,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "It is estabilished fact that a read",
+                ImagemURL = "teste",
+                CategoryName = "T-shirt"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "It is estabilished fact that a read",
+                ImagemURL = "teste",
+                CategoryName = "T-shirt"
+            }); modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 5,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "It is estabilished fact that a read",
+                ImagemURL = "teste",
+                CategoryName = "T-shirt"
+            });
+        }
+
+    }
+}
